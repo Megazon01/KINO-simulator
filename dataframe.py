07f66c0a8321bcc990a63069561d952d584c802c
Petros Zonias <pzonias@hotmail.com>
2024-08-26 17:23:04 +0300

    More Pandas

--- dataframe.py
+++ dataframe.py
@@ -12,6 +12,22 @@ print(df)
 
 print(f"This is how you get Jen's data: {fn.get_Jens_data(df)}")
 
-childrens_column = df['Age']/2
-print(childrens_column)
+
+
+fn.create_column('Car Brand', ['Mazda', 'Ferrari', 'BMW', 'Mercedes'], df)
+print(df)
+
+
+
+while True:
+    inp = input("Enter Client's name: ")
+    if inp in df.Name.tolist():
+        client = df.loc[df['Name'] == inp]
+        print(client.to_string(index=False))
+    elif inp == 'n':
+        break
+    else:
+        print('Invalid Input')
+
+
 #new_df = df("Children's age":[childrens_column])
--- functions.py
+++ functions.py
@@ -4,3 +4,7 @@ import pandas as pd
 def get_Jens_data(df:pd.DataFrame) -> pd.DataFrame:
     result = df.loc[df['Name'] == 'Jen', ['Name', 'Age']]
     return(result)
+
+def create_column(col_name, col_values, df):
+    df[col_name] = col_values
+    return df
